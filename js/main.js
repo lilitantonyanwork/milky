82724732e2007792e7cdc6e184589b05094404b4
lilitantonyan <lilit@helix.am>
2025-03-07 22:03:49 +0400

    add files

--- js/main.js
+++ js/main.js
@@ -3,6 +3,19 @@ var swiper = new Swiper(".review__list", {
     spaceBetween: 31, /* Расстояние между слайдами */
     freeMode: true,
     grabCursor: true,
+    breakpoints: {
+        600:{
+            slidesPerView: 2,
+        },
+        800: {
+            slidesPerView: 2,
+            spaceBetween: 40,
+        },
+        1024: {
+            slidesPerView: 2.5,
+            spaceBetween: 50,
+        },
+    }
 });
 var swiper__img = new Swiper(".master__slider", {
     slidesPerView: 1,
@@ -40,5 +53,55 @@ $(function (){
         $(this).toggleClass('btn__like--active')
         $('#dislikeMaster').addClass('m-modal--active');
         $('html').addClass('m-modal--shown');
+    });
+    $('.btn__info').on('click',function (e){
+        e.preventDefault();
+        $('#aboutInfo').addClass('m-modal--active');
+        $('html').addClass('m-modal--shown');
+    });
+    $('.master__tags--item-more').on('click',function (){
+        $('.master__tags--item').css('display', 'flex');
+        $(this).hide();
+
     })
+    $('.mobile-review__img--count').on('click',function (){
+        console.log('aaaaa')
+        $('.master__info').addClass('hidden');
+        $('.master-about').addClass('hidden');
+        $('.master__inner > .btn__back').addClass('hidden');
+        $('.graphic').addClass('hidden');
+        $('.blitz').addClass('hidden');
+        $('.review').show();
+        $('.mobile-review').addClass('active')
+    })
+    $('.mobile-review__title .btn__back').on('click',function (){
+        $('.master__info').removeClass('hidden');
+        $('.master-about').removeClass('hidden');
+        $('.master__inner > .btn__back').removeClass('hidden');
+        $('.graphic').removeClass('hidden');
+        $('.blitz').removeClass('hidden');
+        $('.review').hide();
+        $('.mobile-review').removeClass('active')
+    });
+    $('.btn__review').on('click', function (e){
+        e.preventDefault();
+        $('.review').hide();
+        $('.gallery').show();
+        $('.gallery').addClass('active');
+        $('.mobile-review ').addClass('hidden').removeClass('active');
+
+    });
+    $('.gallery .btn__back').on('click',function (e){
+        e.preventDefault();
+        $('.review').show();
+        $('.gallery').hide();
+        $('.gallery').removeClass('active');
+        $('.mobile-review ').addClass('active').removeClass('hidden');
+    })
+
+        if( $(window).width() < 769){
+            console.log('aaasdasd')
+            swiper.destroy(true, true)
+        }
+
 })
